663096bf25a05f4604e4d2dad0a44436c0aa5cc3
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-12-11 16:04:35 +0100

    firstTest

--- tests/Codecool.Histogram.UnitTests/TextReaderTests.cs
+++ tests/Codecool.Histogram.UnitTests/TextReaderTests.cs
@@ -1,10 +1,18 @@
 using NUnit.Framework;
+using System.IO;
 
 namespace Codecool.Histogram.UnitTests
 {
     [TestFixture]
     public class TextReaderTests
     {
+        [Test]
+        public void Read_WhenFileNotExist_ThrowException()
+        {
+            const string fileName = "NotFound.txt";
+            TextReader textReader = new TextReader(fileName);
 
+            Assert.Throws<FileNotFoundException>(() => textReader.Read());
+        }
     }
 }
