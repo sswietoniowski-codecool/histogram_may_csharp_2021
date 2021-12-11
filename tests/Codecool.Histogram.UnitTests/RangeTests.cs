01118bad8ba192636f632773e0812236fde97278
Maciej M <matysiewskimaciej@gmail.com>
2021-12-11 16:46:17 +0100

    5th test

--- tests/Codecool.Histogram.UnitTests/RangeTests.cs
+++ tests/Codecool.Histogram.UnitTests/RangeTests.cs
@@ -5,6 +5,17 @@ namespace Codecool.Histogram.UnitTests
     [TestFixture]
     public class RangeTests
     {
-        
+        [Test]
+        [TestCase(1, 4, "the", true)]
+        [TestCase(1, 4, "a", true)]
+        [TestCase(1, 4, "test", true)]
+        [TestCase(1, 4, "testtest", false)]
+        public void IsInRange_WhenCalled_ShouldReturnProperResult(int from, int to, string wordToTest, bool expectedValue)
+        {
+            Range range = new Range(from, to);
+            bool actualValue = range.IsInRange(wordToTest);
+
+            Assert.AreEqual(expectedValue, actualValue);
+        }
     }
 }
